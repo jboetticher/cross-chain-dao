0a670b47bac54b1120bb5bd19a7eaf3e88382c87
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-12-16 10:34:12 -0500

    Added layer zero

--- contracts/CrossChainDAO.sol
+++ contracts/CrossChainDAO.sol
@@ -1,34 +1,71 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Import this file to use console.log
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+/* 
+~~~~~~~ ON OPENZEPPELIN ~~~~~~
+Code is generated from: https://www.openzeppelin.com/contracts
+OpenZeppelin is a widely used smart contract framework that has significatly shaped standards in solidity smart contracts.
+This smart contract relies on its governance and ERC-20 smart contracts for hub-chain DAO actions. The governance smart 
+contracts from OpenZeppelin are also based off of Compound's DAO smart contracts.
 
-    event Withdrawal(uint amount, uint when);
+~~~~~~~~ ON TIMELOCKS ~~~~~~~~
+Notice that it doesn't have a Timelock. A timelock is used to delay the execution of a proposal after it has been completed
+in case actors who disagree with the proposal want to exit the ecosystem before it is implemented. This is a common practice
+and can be seen in giant governance schemes like Polkadot. However, this will not be included for this example for 
+simplicity's sake.
+*/
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+// I think the current gameplan is to make our own IVotes smart contract
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+contract CrossChainDAO is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
+    constructor(IVotes _token)
+        Governor("Moonbeam Example Cross Chain DAO")
+        GovernorSettings(1 /* 1 block */, 5 /* 5 block */, 0)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(4)
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function votingDelay()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
     }
 
-    function withdraw() public {
-        // Uncomment this line to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function votingPeriod()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
 
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
     }
 }
